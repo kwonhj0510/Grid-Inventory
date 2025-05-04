79b7409bf618297697c03235c4f4390506e8d88f
kwonhj0510 <khj96592@gmail.com>
2025-05-05 00:51:00 +0900

    추가 | Inventory Item 범위 초과 확인 및 Item Size 적용

--- Assets/03.Scripts/InventoryController.cs
+++ Assets/03.Scripts/InventoryController.cs
@@ -6,7 +6,8 @@ public class InventoryController : MonoBehaviour
     [HideInInspector]
     public ItemGrid selectItemGrid;
 
-    private InventoryItem selectItem;
+    private InventoryItem selectedItem;
+    private InventoryItem overlapItem;
     RectTransform rectTransform;
 
     [SerializeField] private List<ItemData> items;
@@ -32,47 +33,61 @@ public class InventoryController : MonoBehaviour
 
     private void CreateRandomItem()
     {
-        InventoryItem inventoryItem = Instantiate(itemPrefab, canvasTransform).GetComponent<InventoryItem>();
-        rectTransform = inventoryItem.GetComponent<RectTransform>();
-        selectItem = inventoryItem;
+        InventoryItem inventoryItem = Instantiate(itemPrefab).GetComponent<InventoryItem>();
+        selectedItem = inventoryItem;
 
+        rectTransform = inventoryItem.GetComponent<RectTransform>();
+        rectTransform.SetParent(canvasTransform);
         int selectedItemId = Random.Range(0, items.Count);
         inventoryItem.Set(items[selectedItemId]);
     }
 
     private void LeftMouseButtonPress()
     {
-        Vector2Int tileGridPosition = (selectItemGrid.GetTileGridPosition(Input.mousePosition));    //그리드 셀 위치
-        Debug.Log($"선택한 Cell 위치: {selectItemGrid.GetTileGridPosition(Input.mousePosition)}");
-        if (selectItem == null)
+        Vector2 position = Input.mousePosition;
+
+        if (selectedItem != null)
         {
-            PickUpItem(tileGridPosition);
+            position.x -= (selectedItem.itemData.width - 1) * ItemGrid.tileSizeWidth / 2;
+            position.y += (selectedItem.itemData.height - 1) * ItemGrid.tileSizeHeight / 2;
         }
-        else
-        {
-            PlaceItem(tileGridPosition);
-        }
+
+        Vector2Int tileGridPosition = selectItemGrid.GetTileGridPosition(position);    //그리드 셀 위치
+        Debug.Log($"선택한 Cell 위치: {selectItemGrid.GetTileGridPosition(position)}");
+
+        if (selectedItem == null) { PickUpItem(tileGridPosition); }
+        else { PlaceItem(tileGridPosition); }
     }
 
     private void PlaceItem(Vector2Int tileGridPosition)
     {
-        selectItemGrid.PlaceItem(selectItem, tileGridPosition.x, tileGridPosition.y);   //아이템 배치
-        selectItem = null;
+        bool complete = selectItemGrid.PlaceItem(selectedItem, tileGridPosition.x, tileGridPosition.y, ref overlapItem);   //아이템 배치
+        
+        if (complete)
+        {
+            selectedItem = null;
+            if(overlapItem != null)
+            {
+                selectedItem = overlapItem;
+                overlapItem = null;
+                rectTransform = selectedItem.GetComponent<RectTransform>();
+            }
+        }
     }
 
     private void PickUpItem(Vector2Int tileGridPosition)
     {
-        selectItem = selectItemGrid.PickUpItem(tileGridPosition.x, tileGridPosition.y); //아이템 선택
+        selectedItem = selectItemGrid.PickUpItem(tileGridPosition.x, tileGridPosition.y); //아이템 선택
 
-        if (selectItem != null)
+        if (selectedItem != null)
         {
-            rectTransform = selectItem.GetComponent<RectTransform>();
+            rectTransform = selectedItem.GetComponent<RectTransform>();
         }
     }
 
     private void ItemIconDrag()
     {
-        if (selectItem != null)
+        if (selectedItem != null)
         {
             rectTransform.position = Input.mousePosition;
         }
---
+++ Assets/03.Scripts/InventoryHighlight.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+public class InventoryHighlight : MonoBehaviour
+{
+    [SerializeField] RectTransform highlighter;
+}
